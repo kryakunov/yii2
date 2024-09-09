091b6b736202b10461fe0613792c2601a8bd2f3a
kryakunov <andrey-wz@yandex.ru>
2024-09-09 20:51:08 +0500

    yii with docker

---
+++ migrations/m240909_133400_create_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m240909_133400_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'surname' => $this->string(),
+            'password' => $this->string(),
+            'image' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
